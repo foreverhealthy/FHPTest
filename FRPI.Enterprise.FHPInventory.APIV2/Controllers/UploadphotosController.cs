aa112d470c81e1b54b8cae079871a3583543d490
erico manalo <manaloerico@gmail.com>
2016-06-11 02:20:07 +0800

    dawd

---
+++ FRPI.Enterprise.FHPInventory.APIV2/Controllers/UploadphotosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FRPI.Enterprise.FHPInventory.APIV2.Controllers
+{
+    public class UploadphotosController : Controller
+    {
+        // GET: Uploadphotos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
