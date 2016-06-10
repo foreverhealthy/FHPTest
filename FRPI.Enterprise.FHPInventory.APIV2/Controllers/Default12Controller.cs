810b4c25a378566b0b4d444379b6c98cd7beb2d7
erico manalo <manaloerico@gmail.com>
2016-06-11 01:55:57 +0800

    added new files

---
+++ FRPI.Enterprise.FHPInventory.APIV2/Controllers/Default12Controller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FRPI.Enterprise.FHPInventory.APIV2.Controllers
+{
+    public class Default12Controller : ApiController
+    {
+    }
+}
